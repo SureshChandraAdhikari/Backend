ba391cbc01453c77d9b78adad7d0086400dcd240
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-01-10 16:11:43 +0530

    Api Error response ,asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
 dotenv.config({
     path: './env'
 })
 
 connectDB();
-
-
+// .then( () => {
+//   app.listen(process.env.PORT || 8000, () => {
+//     console.log(`Server is running at  port : ${process.env.PORT}`);
+//   });
+// })
+// .catch((err) => {
+//   console.log("MONGODB connection Failed !!!" , err);
+// })
+app.listen(process.env.PORT || 8000, function (err) {
+  if (err) console.log("MONGODB connection Failed" ,err);
+  console.log(`Server is running at  port : ${process.env.PORT}`);
+});
 
 
 
