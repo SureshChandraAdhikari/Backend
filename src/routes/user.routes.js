8873433e581d28556bb37f68e4ba1e693165e4ad
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-01-15 17:55:35 +0530

    user controller and testing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {registerUser} from '../controllers/user.controller.js';
-
+import { upload } from '../middlewares/multer.middleware.js';
 
 
 
@@ -9,7 +9,20 @@ import {registerUser} from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+
+  registerUser
+);
 
 
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,12 +25,14 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-export { uploadOnCloudinary }
 
-cloudinary.uploader.upload(
-  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) {
-    console.log(result);
-  }
-);
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+//   );
+
+
+  export { uploadOnCloudinary }
